f9b18757a581955e416a03bf15f4c9d6d60b98cb
windyaan <windyanurmaretha@gmail.com>
2024-12-29 18:08:47 +0700

    Menambahkan MahasiswaContainer untuk mengambil data Mahasiswa

--- app/src/main/java/com/example/myapplication/kelas/MahasiswaContainer.kt
+++ app/src/main/java/com/example/myapplication/kelas/MahasiswaContainer.kt
@@ -11,3 +11,18 @@ import retrofit2.Retrofit
 interface AppContainer{
     val kontakRepository: MahasiswaRepository
 }
+
+class MahasiswaContainer : AppContainer{
+    private val baseUrl ="http://10.0.2.2:8000/umyTI/" //localhost diganti ip kalau run di hp
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val kontakRepository: MahasiswaRepository by lazy{
+        NetworkKontakRepository(mahasiswaService)
+    }
+}
