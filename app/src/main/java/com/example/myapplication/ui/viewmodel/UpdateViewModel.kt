f3fd5c2fa5997ec83b5fbcc5b950b99162ce7d95
windyaan <windyanurmaretha@gmail.com>
2024-12-30 14:58:35 +0700

    Menambahkan UpdateViewModel

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+): ViewModel(){
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswabyNim(_nim)
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateMhs(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
