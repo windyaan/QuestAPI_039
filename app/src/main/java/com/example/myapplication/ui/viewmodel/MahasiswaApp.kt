bb258a4f0457236d6c6aba4042ac9ab007052f03
windyaan <windyanurmaretha@gmail.com>
2024-12-30 11:54:29 +0700

    Memindahkan lokasi file

---
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/MahasiswaApp.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.ui.viewmodel
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.myapplication.ui.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        //topBar = {TopAppBar(scrollBehavior = scrollBehavior)},
+    ){
+        Surface (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            PengelolaHalaman()
+        }
+    }
+}
