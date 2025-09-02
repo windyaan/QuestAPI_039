aedfa39575afdf43657cde7463973a99f8f045cd
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-02 19:00:32 +0700

    edit ServiceApiSiswa.kt

--- app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
@@ -1,8 +1,9 @@
 package com.example.mydatasiswa.apiservice
 
+import com.example.mydatasiswa.modeldata.DataSiswa
 import retrofit2.http.GET
 
 interface ServiceApiSiswa {
-    @GET(bacaTeman.php)
-    suspend fun getSiswa(): List<Siswa>
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 }
