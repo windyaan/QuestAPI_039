4d33865a94d2f56269aa32c3b6a2db88ca9f04ab
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-04 16:20:11 +0700

    tambah SiswaTopAppBar

--- app/src/main/java/com/example/mydatasiswa/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/PetaNavigasi.kt
@@ -1,12 +1,24 @@
 package com.example.mydatasiswa.uicontroller
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.mydatasiswa.view.DestinasiHome
+import com.example.mydatasiswa.R
+import com.example.mydatasiswa.uicontroller.route.DestinasiEntry
+import com.example.mydatasiswa.uicontroller.route.DestinasiHome
+import com.example.mydatasiswa.view.EntrySiswaScreen
 import com.example.mydatasiswa.view.HomeScreen
 
 @Composable
@@ -23,7 +35,35 @@ fun HostNavigasi(
     NavHost (navController = navController, startDestination = DestinasiHome.route,
         modifier = Modifier){
         composable(DestinasiHome.route){
-            HomeScreen()
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp){
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
 }
