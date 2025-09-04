2c2094dd33c4beb634a441cb4010c8e2f2d01f9e
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-04 16:19:11 +0700

    tambah fun postDataSiswa

--- app/src/main/java/com/example/mydatasiswa/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/repositori/RepositoryDataSiswa.kt
@@ -5,10 +5,12 @@ import com.example.mydatasiswa.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
 }
 
 class JaringanDataRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>  = serviceApiSiswa.postSiswa (dataSiswa)
     }
