60a133520bd28577bb4af1cb865a57a73f9320a0
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-04 16:38:56 +0700

    pindah route home

---
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.mydatasiswa.uicontroller.route
+
+import com.example.mydatasiswa.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
