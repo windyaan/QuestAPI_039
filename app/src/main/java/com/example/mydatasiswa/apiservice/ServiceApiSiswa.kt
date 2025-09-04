163e66466087ea54ff85e530b5e9c8c5b317db85
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-04 16:19:38 +0700

    tambah method POST

--- app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
@@ -1,9 +1,14 @@
 package com.example.mydatasiswa.apiservice
 
 import com.example.mydatasiswa.modeldata.DataSiswa
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
