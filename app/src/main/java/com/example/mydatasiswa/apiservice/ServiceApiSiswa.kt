f4d24faa49fc59db26078a04900d7b92b6fcc984
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-11 03:03:33 +0700

    tambah method edit dan hapus

--- app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.mydatasiswa.apiservice
 
 import com.example.mydatasiswa.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
@@ -11,4 +14,13 @@ interface ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int): retrofit2.Response<Void>
 }
