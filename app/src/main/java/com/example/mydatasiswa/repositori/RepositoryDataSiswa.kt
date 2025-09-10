f5ede3813692e6b0cef6f2b563c1c4b7cbe85981
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-11 03:07:17 +0700

    tambah edit dan hapus

--- app/src/main/java/com/example/mydatasiswa/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/repositori/RepositoryDataSiswa.kt
@@ -6,6 +6,9 @@ import com.example.mydatasiswa.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int) :retrofit2.Response<Void>
 }
 
 class JaringanDataRepositoryDataSiswa(
@@ -13,4 +16,7 @@ class JaringanDataRepositoryDataSiswa(
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>  = serviceApiSiswa.postSiswa (dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>  = serviceApiSiswa.editSatuSiswa (id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>  = serviceApiSiswa.hapusSatuSiswa(id)
     }
