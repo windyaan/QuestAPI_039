2dec0c865268584d5313dbb0f6199422b52881db
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-11 03:14:41 +0700

    tambah variable untuk check error

--- app/src/main/java/com/example/mydatasiswa/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/mydatasiswa/repositori/ContainerApp.kt
@@ -5,6 +5,8 @@ import com.example.mydatasiswa.apiservice.ServiceApiSiswa
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 interface ContainerApp {
@@ -14,6 +16,12 @@ interface ContainerApp {
 class DefaultContainerApp : ContainerApp{
     private val baseurl = "http://10.0.2.2/umyTI/"
 
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(baseurl)
         .addConverterFactory(
@@ -23,6 +31,7 @@ class DefaultContainerApp : ContainerApp{
                 isLenient = true
             }.asConverterFactory("application/json".toMediaType())
         )
+        .client(klien)
         .build()
 
     private val retrofitService : ServiceApiSiswa by lazy {
