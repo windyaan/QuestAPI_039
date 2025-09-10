0883ca4910594a3a5f76b93b3b329761c7dfa447
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-11 03:10:35 +0700

    tambah intializer untuk akses DetailViewModel

--- app/src/main/java/com/example/mydatasiswa/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mydatasiswa/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.mydatasiswa.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,5 +14,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositoryDataSiswa)}
+        //initializer { EditViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
     }
 }
