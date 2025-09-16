38e5b54c87ded837c7745a40b9fdefbb92cb2fd2
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-16 16:38:42 +0700

    edit PenyediaViewModel.kt

--- app/src/main/java/com/example/mydatasiswa/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/mydatasiswa/viewmodel/PenyediaViewModel.kt
@@ -15,6 +15,6 @@ object PenyediaViewModel {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
         initializer { DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositoryDataSiswa)}
-        //initializer { EditViewModel(aplikasiDataSiswa().container.repositoryDataSiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositoryDataSiswa)}
     }
 }
