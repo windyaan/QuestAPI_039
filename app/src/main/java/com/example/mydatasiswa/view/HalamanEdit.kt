313b8368cc1a30495a8c4d7ebad632f5d6d01043
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-16 16:38:20 +0700

    tambah HalamanEdit.kt

---
+++ app/src/main/java/com/example/mydatasiswa/view/HalamanEdit.kt
@@ -0,0 +1,49 @@
+//package com.example.mydatasiswa.view
+//
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.ExperimentalMaterial3Api
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.rememberCoroutineScope
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.res.stringResource
+//import androidx.lifecycle.viewmodel.compose.viewModel
+//import com.example.mydatasiswa.uicontroller.SiswaTopAppBar
+//import com.example.mydatasiswa.uicontroller.route.DestinasiEdit
+//import com.example.mydatasiswa.viewmodel.EditViewModel
+//import com.example.mydatasiswa.viewmodel.PenyediaViewModel
+//import kotlinx.coroutines.launch
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun EditSiswaScreen(
+//    navigateBack: () -> Unit,
+//    onNavigateUp: () -> Unit,
+//    modifier: Modifier = Modifier,
+//    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+//) {
+//    Scaffold(
+//        topBar = {
+//            SiswaTopAppBar(
+//                title = stringResource(DestinasiEdit.titleRes),
+//                canNavigateBack = true,
+//                navigateUp = onNavigateUp
+//            )
+//        },
+//        modifier = modifier
+//    ) { innerPadding ->
+//        val coroutineScope = rememberCoroutineScope()
+//
+//        EntrySiswaBody(
+//            uiStateSiswa = viewModel.uiStateSiswa,
+//            onSiswaValueChange = viewModel::updateUiState,
+//            onSaveClick = {
+//                coroutineScope.launch {
+//                    viewModel.editSatuSiswa()
+//                    navigateBack()
+//                }
+//            },
+//            modifier = Modifier.padding(innerPadding)
+//        )
+//    }
+//}
