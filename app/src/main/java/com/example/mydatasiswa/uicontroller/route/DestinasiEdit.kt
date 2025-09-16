d7081ea7ab665055388a428373231aac2d66f34d
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-09-16 16:37:37 +0700

    tambah DestinasiEdit.kt

--- app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/mydatasiswa/uicontroller/route/DestinasiEdit.kt
@@ -1,10 +1,10 @@
-package com.example.mydatasiswa.uicontroller.route
-
-import com.example.mydatasiswa.R
-
-object DestinasiEdit: DestinasiNavigasi {
-    override val route = "item_edit"
-    override val titleRes = R.string.edit_siswa
-    const val itemIdArg = "idSiswa"
-    val routeWithArg = "$route/{$itemIdArg}"
-}
+//package com.example.mydatasiswa.uicontroller.route
+//
+//import com.example.mydatasiswa.R
+//
+//object DestinasiEdit: DestinasiNavigasi {
+//    override val route = "item_edit"
+//    override val titleRes = R.string.edit_siswa
+//    const val itemIdArg = "idSiswa"
+//    val routeWithArg = "$route/{$itemIdArg}"
+//}
